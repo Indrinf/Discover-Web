3cf511f84e13dc156d7ac676f281072b0f6be9c2
Indrinf <indrinurfiani99@gmail.com>
2023-05-14 18:56:09 +0700

    add template

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/master', function(){
+    return view('layout.master');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
