bebd2406fa465eabe68dc165105029a1b010407b
Indrinf <indrinurfiani99@gmail.com>
2023-05-14 19:28:22 +0700

    split to partial

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -46,68 +46,11 @@ https://templatemo.com/tm-582-tale-seo-agency
   <!-- ***** Preloader End ***** -->
 
   <!-- ***** Pre-Header Area Start ***** -->
-  <div class="pre-header">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-sm-9">
-          <div class="left-info">
-            <ul>
-              <li><a href="#"><i class="fa fa-phone"></i>+000 1234 5678</a></li>
-              <li><a href="#"><i class="fa fa-envelope"></i>infocompany@email.com</a></li>
-              <li><a href="#"><i class="fa fa-map-marker"></i>Bandung, Indonesia</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-lg-4 col-sm-3">
-          <div class="social-icons">
-            <ul>
-              <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-              <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-              <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
-              <li><a href="#"><i class="fab fa-google-plus"></i></a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  @include('partial.preheader')
   <!-- ***** Pre-Header Area End ***** -->
 
   <!-- ***** Header Area Start ***** -->
-  <header class="header-area header-sticky">
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <nav class="main-nav">
-            <!-- ***** Logo Start ***** -->
-            <a href="index.html" class="logo">
-              <img src="{{asset('/layout/assets/images/logo.png')}}" alt="" style="max-width: 112px;">
-            </a>
-            <!-- ***** Logo End ***** -->
-            <!-- ***** Menu Start ***** -->
-            <ul class="nav">
-              <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
-              <li class="scroll-to-section"><a href="#services">Services</a></li>
-              <li class="scroll-to-section"><a href="#projects">Projects</a></li>
-              <li class="has-sub">
-                <a href="javascript:void(0)">Pages</a>
-                <ul class="sub-menu">
-                  <li><a href="about.html">About Us</a></li>
-                  <li><a href="faqs.html">FAQs</a></li>
-                </ul>
-              </li>
-              <li class="scroll-to-section"><a href="#infos">Infos</a></li>
-              <li class="scroll-to-section"><a href="#contact">Login</a></li>
-            </ul>
-            <a class='menu-trigger'>
-              <span>Menu</span>
-            </a>
-            <!-- ***** Menu End ***** -->
-          </nav>
-        </div>
-      </div>
-    </div>
-  </header>
+  @include('partial.header')
   <!-- ***** Header Area End ***** -->
 
   <div class="main-banner" id="top">
@@ -379,17 +322,8 @@ https://templatemo.com/tm-582-tale-seo-agency
     </div>
   </div>
 
-  <footer>
-    <div class="container">
-      <div class="col-lg-12">
-        <p>Copyright © 2036 <a href="#">Tale SEO Agency</a>. All rights reserved.
-
-          <br>Design: <a href="https://templatemo.com" target="_blank">TemplateMo</a> Distribution: <a
-            href="https://themewagon.com">ThemeWagon</a>
-        </p>
-      </div>
-    </div>
-  </footer>
+  <!-- footer -->
+  @include('partial.footer')
 
 
   <!-- Scripts -->
